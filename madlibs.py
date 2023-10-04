e64921de31b7e1ed9dddfdb33358f2c08b85c519
Rammo17 <143664020+Rammo17@users.noreply.github.com>
2023-10-04 18:25:11 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+<html>
+<body>
+<script>
+var noun = 'spaghetti' ;
+var adjective = 'delirious' ;
+var adverb = 'abruptly'
+var verb = 'grovelling'
+"On a tropical" + "noun",
+"Underneath a" + "adjective" + "noun".
+"Hangin' with the" + "noun",
+"Askin' questions cause' they got all the" + "noun".
+</script>
+</body>
+</html> 
