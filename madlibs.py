458d311f11db384711a07a9ecfa320fbac20cfc3
Rammo17 <ramosamatthew1@gmail.com>
2023-10-04 20:37:30 -0400

    updated all

---
+++ madlibs.py
@@ -0,0 +1,3 @@
+noun = input("Give me a noun: ")
+adjective = input("Give me an adjective: ")
+print("On a tropical " + str(noun) + " " + "Underneath a " + str(adjective) + " " + str(noun) + " " + "Hangin' with the " + str(noun) + " " + "Askin' questions cause' they got all the " + str(noun) + ".")
